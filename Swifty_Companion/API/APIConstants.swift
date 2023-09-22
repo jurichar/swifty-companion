5755b2e2d82aa2c2fdca8473a7b86dc95a361ede
Julien Richard <julienrichard98@gmail.com>
2023-09-22 15:03:18 +0200

    Update APIConstants.swift

--- Swifty_Companion/API/APIConstants.swift
+++ Swifty_Companion/API/APIConstants.swift
@@ -5,5 +5,9 @@
 //  Created by Julien Richard on 06/08/2023.
 //
 
-API_URL= SHHEEEEEEEEEESSSHHH jte bz <3 
-
+API_URL= Oh non ya plus la clef
+API_42= Jte fais l'amour
+API = Suce moi les boules <3
+secret_42= Jfais la bise a tes parents 
+super_secret= Jte fais un petit frère
+X= Oui c'est bien a toi que je parle ;)
