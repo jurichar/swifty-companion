3442e14400fa6fc1e646bd8d2e9b5e5023f00679
Julien Richard <julienrichard98@gmail.com>
2023-09-22 12:11:32 +0200

    Update APIConstants.swift

--- Swifty_Companion/API/APIConstants.swift
+++ Swifty_Companion/API/APIConstants.swift
@@ -5,10 +5,5 @@
 //  Created by Julien Richard on 06/08/2023.
 //
 
-import Foundation
-struct APIConstants {
-    static let baseURL = "https://api.intra.42.fr"
-    static let UID = "u-s4t2ud-2cebaec63eb9e0268e98cd7de6451e6f748b33628bb151fedacff063ccdddfdf"
-    static let secret = "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"
-}
+import 
 
